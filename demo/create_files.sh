08269ac79f86aab11ff00c3a17d473911ce80ccf
Dennis Noll <dennis.noll@cern.ch>
2025-10-01 17:52:26 -0700

    [dev] change demo data

--- demo/create_files.sh
+++
@@ -1,9 +0,0 @@
-for param0 in a b c
-do
-    for param1 in 1 2 3
-    do
-        tp="param0_${param0}/param1_${param1}"
-        mkdir -p $tp
-        echo "${param0} ${param1}" >> "${tp}/file.txt"
-    done
-done
